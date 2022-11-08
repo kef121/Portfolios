fd4a157de595c4ae25462016dd13c6c6410be397
favour <favourokeke958@gmail.com>
2022-11-09 00:18:46 +0100

    style: Services section styling

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import {BsFillMoonStarsFill} from "react-icons/bs";
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import Image from "next/image"
 import deved from "../public/dev-ed-wave.png"
+import design from "../public/design.png"
+import code from "../public/code.png"
+import consulting from "../public/consulting.png"
 
 
 export default function Home() {
@@ -41,6 +44,53 @@ export default function Home() {
 
           <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
             <Image src={deved} layout="fill" objectFit='cover' />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1 '>Services I offer</h3>
+            <p className='text-md py-2 leading-8 text-gray-800'>Since the beginning of my journey as a freelance developer,
+               I've done remote work for <span className='text-teal-500'>agencies</span> consulted 
+               for <span className='text-teal-500'>startups</span> and collaborated with talented people to create digital products
+               for both business and consumer use.</p>
+            <p className='text-md py-2 leading-8 text-gray-800'>Since the beginning of my journey as a freelance developer,
+               I've done remote work for a long time.</p>
+          </div>
+
+          <div>
+            <div className='grid place-items-center text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={design} width={100} height={100}  />
+              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+              <p className='py-2'>Creating elegant designs suited for your 
+                needs following core design theory.</p>
+              <h4 className='text-teal-600 py-4'>Design tools i use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+
+            <div className='grid place-items-center text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={code} width={100} height={100}  />
+              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+              <p className='py-2'>Creating elegant designs suited for your 
+                needs following core design theory.</p>
+              <h4 className='text-teal-600 py-4'>Design tools i use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+
+            <div className='grid place-items-center text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={consulting} width={100} height={100}  />
+              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+              <p className='py-2'>Creating elegant designs suited for your 
+                needs following core design theory.</p>
+              <h4 className='text-teal-600 py-4'>Design tools i use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
           </div>
         </section>
       </main>
